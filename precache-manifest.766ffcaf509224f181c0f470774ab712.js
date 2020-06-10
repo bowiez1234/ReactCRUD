98daf3909d8703310382dd9a3e38e640d8ea9112
bowiez1234@gmail.com <bowiez1234@gmail.com>
2020-06-10 18:12:55 +0700

    Updates

---
+++ precache-manifest.766ffcaf509224f181c0f470774ab712.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c3313e256e7ecd0ec00ca9b7f426eed",
+    "url": "/ReactCRUD/index.html"
+  },
+  {
+    "revision": "f0d04d8cdc41567eeb35",
+    "url": "/ReactCRUD/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f0d04d8cdc41567eeb35",
+    "url": "/ReactCRUD/static/js/2.c7e669da.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/ReactCRUD/static/js/2.c7e669da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffb392efc9a79cac71c8",
+    "url": "/ReactCRUD/static/js/main.8704866a.chunk.js"
+  },
+  {
+    "revision": "a8a135fea52fce56035d",
+    "url": "/ReactCRUD/static/js/runtime-main.2399f485.js"
+  }
+]);
